30410154d62bb2a682fe623c8d8ebcab98f442e9
Yahor Simak <simak_yahor@mail.ru>
2018-04-03 22:43:07 +0300

    add some components

--- logic/components/addArticleBtn.js
+++ logic/components/addArticleBtn.js
@@ -1,12 +1,10 @@
 var AddArticleBtnController = function($location) {
-   
     this.redirect = function() {
         $location.path('/article/add');
     }
 }
 
 AddArticleBtnController.$inject = ['$location'];
-
 angular.module('testApp').component('addArticleBtn', {
     template: '<button class="btn btn-dark" ng-click="$ctrl.redirect()">Add article</button>',
     controller: AddArticleBtnController
